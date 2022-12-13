868bb72d147e8a4d72203669608875317a316381
jwhong <jwhong98@gmail.com>
2022-12-13 14:57:36 -0800

    completed layout for Education

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import classes from "../styles/Education.module.css";
+import CVForm from "./CVForm";
+import ListItem from "./ListItem";
 
 const Education = () => {
-  return <div>Education</div>;
+  const [education, setEducation] = useState([]);
+  const [listing, setListing] = useState({
+    start: "",
+    end: "",
+    degree: "",
+    institute: "",
+  });
+  const createListItem = (info) => {
+    return (
+      <ListItem
+        start={info.start}
+        end={info.end}
+        degree={info.degree}
+        institute={info.institute}
+      />
+    );
+  };
+  return (
+    <section className={classes.education}>
+      <h2>Education</h2>
+      <div className={classes.educationContainer}>
+        {education.map(createListItem)}
+      </div>
+      <CVForm data={education} />
+    </section>
+  );
 };
 
 export default Education;
---
+++ src/components/ListItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "../styles/ListItem.module.css";
+
+const ListItem = (props) => {
+  return (
+    <div className={classes.listItem}>
+      <div className={classes.dates}>
+        {props.start} - {props.end}
+      </div>
+      <div className={classes.schoolInfo}>
+        <p className={classes.degree}>{props.degree}</p>
+        <p className={classes.institute}>{props.institute}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ListItem;
