fe10194eed9b01a9ae041e05c5119487ce166be8
jwhong <jwhong98@gmail.com>
2022-12-13 11:30:28 -0800

    Base file structure

---
+++ src/components/CVForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CVForm = () => {
+  return <div>CVForm</div>;
+};
+
+export default CVForm;
---
+++ src/components/EditBtn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditBtn = () => {
+  return <div>EditBtn</div>;
+};
+
+export default EditBtn;
